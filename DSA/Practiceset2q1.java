8c97084eba0f31718345cdabd3c17460015a6cba
Divej Ahuja <147057830+DIVEJ131203@users.noreply.github.com>
2024-02-04 12:43:06 +0530

    dsa java

---
+++ DSA/Practiceset2q1.java
@@ -0,0 +1,8 @@
+public class Practiceset2q1 {
+    public static void main(String[] args) {
+        int x = 200, y = 50;
+        int ex1 = (x * y / x);
+        int ex2 = (x * (y / x));
+        System.out.println(ex1 + "," + ex2);
+    }
+}
